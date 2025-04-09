b351b7a6a2043cbcd0222387f99f1292ec62deea
purange <jumokcom2001@gmail.com>
2025-04-09 11:14:06 +0900

    fix: 검색 페이지에 Suspense 추가

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,6 @@
 import { getAllPosts } from '@/lib/posts';
 import { SearchClient } from './SearchClient';
+import { Suspense } from 'react';
 
 // 서버에서 데이터 가져오기
 async function getPosts() {
@@ -8,5 +9,9 @@ async function getPosts() {
 
 export default async function SearchPage() {
   const posts = await getPosts();
-  return <SearchClient initialPosts={posts} />;
+  return (
+    <Suspense fallback={<div>검색 중...</div>}>
+      <SearchClient initialPosts={posts} />
+    </Suspense>
+  );
 } 
